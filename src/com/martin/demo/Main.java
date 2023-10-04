07ff56620d871df48abe618653d4c8c774f39d29
unknown <martin.jernstrom1@gmail.com>
2023-10-04 20:22:22 +0200

    task

--- src/com/martin/demo/Main.java
+++ src/com/martin/demo/Main.java
@@ -1,4 +1,20 @@
 package com.martin.demo;
 
 public class Main {
+    public static void main(String[] args) {
+        devZero();
+
+    }
+
+    public static void devZero(){
+
+        try{
+            System.out.println(2/0);
+        }catch (Exception error){
+            error.printStackTrace();
+        }
+
+        System.out.println("after crash");
+
+    }
 }
