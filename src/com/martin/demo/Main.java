fdd8f987e0970aeccd408e68db17dc74907f1673
unknown <martin.jernstrom1@gmail.com>
2023-10-04 20:23:55 +0200

    uppgift1

--- src/com/martin/demo/Main.java
+++ src/com/martin/demo/Main.java
@@ -2,7 +2,13 @@ package com.martin.demo;
 
 public class Main {
     public static void main(String[] args) {
-        devZero();
+
+        int forlooper = 0;
+
+        for (int i = 0; i < 10; i++) {
+
+            forlooper += i;
+        }
 
     }
 
